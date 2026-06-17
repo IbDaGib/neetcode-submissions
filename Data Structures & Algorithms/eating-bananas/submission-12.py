ec1056ef9dad550d16a46058411755f774445b9c
Muhammad Ibrahim <65189418+IbDaGib@users.noreply.github.com>
2026-06-16 21:39:03 -0400

    Add: eating-bananas - submission-12

---
+++ Data Structures & Algorithms/eating-bananas/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+
+        while l <= r:
+            m = (l+r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/m)
+            
+            if hours <= h:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return l
