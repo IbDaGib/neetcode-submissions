cc971f2cd75bf22b0cf09b8eb0d0f4cd28ad51fa
Muhammad Ibrahim <65189418+IbDaGib@users.noreply.github.com>
2026-07-27 14:12:17 -0400

    Add: subtree-of-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-6.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.same(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def same(self, r1, r2):
+        if not r1 and not r2:
+            return True
+        if not r1 or not r2:
+            return False
+        if r1.val == r2.val:
+            return (self.same(r1.left, r2.left) and self.same(r1.right, r2.right))
+        else:
+            return False
